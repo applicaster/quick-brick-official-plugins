4be499aa4d203ecae48b0c560bd75b31913b2977
Alex Khomich <khomich@scand.com>
2020-04-20 13:54:04 +0300

    [Android] MRP-600: Add Google AdMod interstitials plugin.

---
+++ plugins/quick-brick-google-admob-interstitials/android/src/main/java/com/applicaster/google_admob_interstitials_qb/AdListenerImpl.kt
@@ -0,0 +1,26 @@
+package com.applicaster.google_admob_interstitials_qb
+
+import com.google.android.gms.ads.AdListener
+
+class AdListenerImpl : AdListener() {
+	override fun onAdImpression() {
+	}
+
+	override fun onAdLeftApplication() {
+	}
+
+	override fun onAdClicked() {
+	}
+
+	override fun onAdFailedToLoad(errorCode: Int) {
+	}
+
+	override fun onAdClosed() {
+	}
+
+	override fun onAdOpened() {
+	}
+
+	override fun onAdLoaded() {
+	}
+}
