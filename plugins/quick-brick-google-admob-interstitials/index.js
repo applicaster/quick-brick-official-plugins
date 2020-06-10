dbde7b3d309b92f477e28d03d8d3625d773051e3
oduk <oduk@scand.com>
2020-06-10 14:17:35 +0300

    [QuickBrick] TPS-200: added reducer to obtain advertising data. Added hook interface.

--- plugins/quick-brick-google-admob-interstitials/index.js
+++ plugins/quick-brick-google-admob-interstitials/index.js
@@ -1,25 +1,32 @@
-import { withNavigator } from '@applicaster/zapp-react-native-ui-components/Decorators/Navigator/';
 import { connectToStore } from '@applicaster/zapp-react-native-redux';
 import Interstitial from './src/Interstitial';
-import InlineBanner from './src/InlineBanner';
+
 
 const storeConnector = connectToStore((state) => { // Store connector entity to obtain screen data
   const values = Object.values(state.rivers);
-  const screenData = values.find(
-    ({ type }) => type === 'google-admob-interstitials-qb'
-  );
-  return { screenData };
+
+  const hasAdmobPrehookReducer = (accumulator, item) => {
+    // eslint-disable-next-line camelcase
+    const preloadPlugins = item?.hooks?.preload_plugins;
+    const isNotEmptyArray = Array.isArray(preloadPlugins) && preloadPlugins.length > 0;
+    if (!isNotEmptyArray) return accumulator;
+
+    const hasAdmobPrehook = preloadPlugins.some(({ identifier }) => identifier === 'google-admob-ads-qb');
+    if (hasAdmobPrehook) {
+      accumulator[item.id] = item?.advertising;
+    }
+    return accumulator;
+  };
+
+  const advertisingData = values.reduce(hasAdmobPrehookReducer, {});
+  return { advertisingData };
 });
 
-function AdMobPlugin(props) {
-  console.log(props);
 
-  const {
-    onLoadFinished,
-    screenData
-  } = props;
+const InterstitialAd = {
+  Component: storeConnector(Interstitial),
+  isFlowBlocker: () => true,
+  presentFullScreen: true
+};
 
-  return onLoadFinished ? InlineBanner(props) : Interstitial(screenData);
-}
-
-export default storeConnector(AdMobPlugin);
+export default InterstitialAd;
