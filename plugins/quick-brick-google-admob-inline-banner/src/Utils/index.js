2012377995bda412039175aa64f940b59f9db9fc
oduk <oduk@scand.com>
2020-06-18 09:54:09 +0300

    [QuickBrick] added show background only after loading an ad. Refactored code.

--- plugins/quick-brick-google-admob-inline-banner/src/Utils/index.js
+++ plugins/quick-brick-google-admob-inline-banner/src/Utils/index.js
@@ -12,19 +12,27 @@ const isAndroid = Platform.OS === 'android';
 
 function getBannerSize(bannerAdType) {
   const standartBanner = isTablet ? BannerAdSize.LEADERBOARD : BannerAdSize.BANNER;
+  // FLUID banners are not rendered on Android
+  const adaptiveBanner = isAndroid ? BannerAdSize.SMART_BANNER : BannerAdSize.FLUID;
 
   const banners = {
     [bannerTypes.standard]: standartBanner,
     [bannerTypes.smart]: BannerAdSize.SMART_BANNER,
-    [bannerTypes.adaptive]: BannerAdSize.FLUID,
+    [bannerTypes.adaptive]: adaptiveBanner,
     [bannerTypes.box]: BannerAdSize.MEDIUM_RECTANGLE
   };
 
-  return banners[bannerAdType];
+  return banners[bannerAdType] || standartBanner;
+}
+
+function getBannersWithBackground(bannerAdType) {
+  return bannerAdType === bannerTypes.standard
+    || bannerAdType === bannerTypes.box;
 }
 
 export {
   isTablet,
   isAndroid,
-  getBannerSize
+  getBannerSize,
+  getBannersWithBackground
 };
