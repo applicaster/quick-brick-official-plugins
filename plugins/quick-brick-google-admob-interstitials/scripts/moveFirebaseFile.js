97d1b18b486a856d16cbd5be178398278e2e65d4
oduk <oduk@scand.com>
2020-06-26 18:37:14 +0300

    [QuickBrick] TPS-301: updated firebase dependency. Added postinstall script. Bumped version 0.1.3 interstitials.

---
+++ plugins/quick-brick-google-admob-interstitials/scripts/moveFirebaseFile.js
@@ -0,0 +1,44 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const pluginIdentifier = 'google-admob-ads-qb';
+
+const targetFolderPath = path.join('..', '..', '..', '..', '..', 'firebase.json'); // put firebase.json on the same level with zapp-platform-android
+const targetRootPath = path.join('..', '..', '..', '..', 'firebase.json'); // put firebase.json to the root of zapp-platform-android
+const pathToPluginConfig = path.join('..', '..', '..', '..', 'config', 'android', 'plugin_configurations.json');
+
+
+// eslint-disable-next-line consistent-return
+function getConfigFile(dest, id) {
+  try {
+    // eslint-disable-next-line global-require,import/no-dynamic-require
+    const pluginConfig = require(dest);
+
+    const firebasePlugin = pluginConfig.find((item) => item.plugin.identifier === id);
+    return firebasePlugin.configuration_json.admob_configuration_json;
+  } catch (err) {
+    console.log('Cannot find path to config file');
+  }
+}
+
+function download(url, dest, cb) {
+  fs.access(dest, fs.F_OK, (err) => {
+    if (err) {
+      const file = fs.createWriteStream(dest);
+      https.get(url, (response) => {
+        response.pipe(file);
+        file.on('finish', () => {
+          file.close(cb);
+        });
+      });
+    }
+  });
+}
+
+const configFileUrl = getConfigFile(pathToPluginConfig, pluginIdentifier);
+
+if (configFileUrl) {
+  download(configFileUrl, targetFolderPath);
+  download(configFileUrl, targetRootPath);
+}
