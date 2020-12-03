5fe3ffc7046d3e90b19459d113f4f2ade1d307f9
Alex <24409942+inckie@users.noreply.github.com>
2020-12-03 15:47:34 -0500

    feat: generate Firebase config json file from plugin configuration

--- plugins/quick-brick-google-admob-inline-banner/scripts/moveFirebaseFile.js
+++ plugins/quick-brick-google-admob-inline-banner/scripts/moveFirebaseFile.js
@@ -1,44 +1,63 @@
-const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
 const pluginIdentifier = 'google-admob-inline-banner-qb';
 
-const targetFolderPath = path.join('..', '..', '..', '..', '..', 'firebase.json'); // put firebase.json on the same level with zapp-platform-android
-const targetRootPath = path.join('..', '..', '..', '..', 'firebase.json'); // put firebase.json to the root of zapp-platform-android
+// put firebase.json to the root of zapp-platform-android
+const targetRootPath = path.join('..', '..', '..', '..', 'firebase.json');
 const pathToPluginConfig = path.join('..', '..', '..', '..', 'config', 'android', 'plugin_configurations.json');
 
+var json_body = null;
 
 // eslint-disable-next-line consistent-return
-function getConfigFile(dest, id) {
+function getConfigFile(pluginConfigurationFile, pluginId) {
+
+  if (null != null) {
+    return json_body;
+  }
+
   try {
     // eslint-disable-next-line global-require,import/no-dynamic-require
-    const pluginConfig = require(dest);
+    const pluginConfig = require(pluginConfigurationFile);
 
-    const firebasePlugin = pluginConfig.find((item) => item.plugin.identifier === id);
-    return firebasePlugin.configuration_json.admob_configuration_json;
+    if (null == pluginConfig) {
+      throw `failed to find plugin configuration file ${pluginConfigurationFile}`
+    }
+
+    const firebasePlugin = pluginConfig.find((item) => item.plugin.identifier === pluginId);
+
+    if (null == firebasePlugin.configuration_json) {
+      throw `failed to find plugin ${pluginId} configuration in ${pluginConfigurationFile}`
+    }
+
+    if (null == firebasePlugin.configuration_json.admob_android_app_id) {
+      throw "admob_android_app_id is missing";
+    }
+
+    json_body = {
+      "react-native": {
+        "admob_android_app_id": firebasePlugin.configuration_json.admob_android_app_id,
+      }
+    }
+    return json_body;
   } catch (err) {
-    console.log('Cannot find path to config file');
+    console.error('Cannot find path to config file');
+    throw err;
   }
 }
 
-function download(url, dest, cb) {
+function writeConfig(dest) {
   fs.access(dest, fs.F_OK, (err) => {
     if (err) {
-      const file = fs.createWriteStream(dest);
-      https.get(url, (response) => {
-        response.pipe(file);
-        file.on('finish', () => {
-          file.close(cb);
-        });
+      const json_body = getConfigFile(pathToPluginConfig, pluginIdentifier);
+      fs.writeFile(dest, JSON.stringify(json_body), function (err) {
+        if (err) throw err;
+        console.log(`Saved Firebase config to ${dest}!`);
       });
     }
   });
 }
 
-const configFileUrl = getConfigFile(pathToPluginConfig, pluginIdentifier);
+// todo: call only for Android
 
-if (configFileUrl) {
-  download(configFileUrl, targetFolderPath);
-  download(configFileUrl, targetRootPath);
-}
+writeConfig(targetRootPath);
